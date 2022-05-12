9edfdc759c35f76f0be828a83b7724f0935708d8
Genes Saret Johnes <geneshsarretsarretret@gmail.com>
2022-05-12 19:53:20 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,92 +1,102 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>Genesaret Johnes | Portfolio</title>
     <link rel="icon" type="image/x-icon" href="logos/tech-eye.png">
     <link rel="stylesheet" type="text/css" href="styles.css">
-  </head>
-  <body>
+</head>
+
+<body>
     <header>
-      <h1>Genesaret Johnes</h1>
+        <h1>Genesaret Johnes</h1>
         <ul class="nav">
-          <li class="nav"><a class="navigate" href="http://www.gensprog.blogspot.com">About</a></li>
-          <li class="nav"><a class="navigate" href="mailto:gensart.ai@gmail.com">Contact</a></li>
-          <li class="nav"><a class="navigate" href="">Services</a></li>
-          <li class="nav"><a class="navigate" href="">Home</a></li>
+            <li class="nav"><a class="navigate" href="http://www.gensprog.blogspot.com">About</a></li>
+            <li class="nav"><a class="navigate" href="mailto:gensart.ai@gmail.com">Contact</a></li>
+            <li class="nav"><a class="navigate" href="">Services</a></li>
+            <li class="nav"><a class="navigate" href="">Home</a></li>
         </ul>
     </header>
     <div class="first-section">
-      <div class="left-fsection">
-        <p class="first-p">I'm on learning HTML & CSS</p>
-        <button class="email-button">Email me</button>
-        <p class="contact-links"><a class="linkedin-link" href="https://www.linkedin.com/in/genesaret4252">Linkedin</a> / <a class="instagram-link" href="https://www.instagram.com/gensart.ai/">Instagram</a> / <a class="facebook-link" href="https://www.facebook.com/GenesSaretJohnes">Facebook</a></p>
-      </div>
-      <div class="right-fsection">
-        <img class="gens-potrait" src="images/gens-potrait.jpg" alt="potrait of genes">
-      </div>
+        <div class="left-fsection">
+            <p class="first-p">I'm on learning HTML & CSS</p>
+            <button class="email-button">Email me</button>
+            <p class="contact-links"><a class="linkedin-link"
+                    href="https://www.linkedin.com/in/genesaret4252">Linkedin</a> / <a class="instagram-link"
+                    href="https://www.instagram.com/gensart.ai/">Instagram</a> / <a class="facebook-link"
+                    href="https://www.facebook.com/GenesSaretJohnes">Facebook</a></p>
+        </div>
+        <div class="right-fsection">
+            <img class="gens-potrait" src="images/gens-potrait.jpg" alt="potrait of genes">
+        </div>
     </div>
     <div class="second-section">
-      <div class="left-secsection">
-        <h2>> My Background</h2>
-        <p class="my-background">
-          Oriented to information technology systems, self-motivation, and prospects. IT students with extensive motivation and knowledge and have experience in resolving conflicts with customers. Thorough and considerate in all professional dealings.
-        </p>
-      </div>
-      <div class="right-secsection">
-        <h2>> Software Skills</h2>
-        <ul class="skill-lists">
-          <li>Adobe Photoshop</li>
-          <li>Adobe Premiere Pro</li>
-          <li>Microsoft Office Word</li>
-          <li>Microsoft Office Excel</li>
-          <li>Microsoft Office PowerPoint</li>
-          <li>Visual Studio Code</li>
-          <li>Java / Kotlin Basics</li>
-        </ul>
-      </div>
+        <div class="left-secsection">
+            <h2>> My Background</h2>
+            <p class="my-background">
+                Oriented to information technology systems, self-motivation, and prospects. IT students with extensive
+                motivation and knowledge and have experience in resolving conflicts with customers. Thorough and
+                considerate in all professional dealings.
+            </p>
+        </div>
+        <div class="right-secsection">
+            <h2>> Software Skills</h2>
+            <ul class="skill-lists">
+                <li>Adobe Photoshop</li>
+                <li>Adobe Premiere Pro</li>
+                <li>Microsoft Office Word</li>
+                <li>Microsoft Office Excel</li>
+                <li>Microsoft Office PowerPoint</li>
+                <li>Visual Studio Code</li>
+                <li>Java / Kotlin Basics</li>
+            </ul>
+        </div>
     </div>
     <div class="third-section">
-      <p class="quotes">"on accepting things
-      that may can't be changed."</p>
-      <p class="quotes-maker">~ i don't know who made this quote</p>
+        <p class="quotes">"on accepting things
+            that may can't be changed."</p>
+        <p class="quotes-maker">~ i don't know who made this quote</p>
     </div>
     <div class="fourth-section">
-      <h1>What They Say :</h1>
-      <div class="first-fourth-section">
-        <img class="wts-image" src="images/person1.jpg">
-        <h2 class="wts-header">- Ma Sherwlonk</h2>
-        <p class="wts-content">Ilmu yang ditentukan oleh hanya satu orang tidak akan dapat membuka ilmu terapan lainnya. Semua manusia perlu dibimbing dengan baik agar kelak jati hidupnya menjadi lebih baik.</p>
-      </div>
-      <div class="second-fourth-section">
-        <img class="wts-image" src="images/person2.jpg">
-        <h2 class="wts-header">- Sasori Hirusina</h2>
-        <p class="wts-content">Tiada rasa yang dapat menggantikan rasa keingintahuan dari seorang awam kepada ilmu pengetahuan. Maka berbahagialah orang yang haus akan ilmu pengetahuan.</p>
-      </div>
-      <div class="third-fourth-section">
-        <img class="wts-image" src="images/person3.jpg">
-        <h2 class="wts-header">- Alan Stary</h2>
-        <p class="wts-content">Otak sebagai kekuatan. Hati sebagai pembatas. Otak dan Hati manusia adalah Yin & Yang dalam masa kehidupannya. Semua kembali kepada manusia itu sendiri.</p>
-      </div>
+        <h1>What They Say :</h1>
+        <div class="first-fourth-section">
+            <img class="wts-image" src="images/person1.jpg">
+            <h2 class="wts-header">- Ma Sherwlonk</h2>
+            <p class="wts-content">Ilmu yang ditentukan oleh hanya satu orang tidak akan dapat membuka ilmu terapan
+                lainnya. Semua manusia perlu dibimbing dengan baik agar kelak jati hidupnya menjadi lebih baik.</p>
+        </div>
+        <div class="second-fourth-section">
+            <img class="wts-image" src="images/person2.jpg">
+            <h2 class="wts-header">- Sasori Hirusina</h2>
+            <p class="wts-content">Tiada rasa yang dapat menggantikan rasa keingintahuan dari seorang awam kepada ilmu
+                pengetahuan. Maka berbahagialah orang yang haus akan ilmu pengetahuan.</p>
+        </div>
+        <div class="third-fourth-section">
+            <img class="wts-image" src="images/person3.jpg">
+            <h2 class="wts-header">- Alan Stary</h2>
+            <p class="wts-content">Otak sebagai kekuatan. Hati sebagai pembatas. Otak dan Hati manusia adalah Yin & Yang
+                dalam masa kehidupannya. Semua kembali kepada manusia itu sendiri.</p>
+        </div>
     </div>
     <div class="fifth-section">
-      <div class="first-fifth-section">
-        <form class="form">
-          <h1 class="form-title">Get in touch with me !</h1>
-          <label class="form-name">Your Name</label><br/>
-          <input class="form-input" type="text" placeholder="Fullname"><br/><br/>
-          <label class="form-email">Your Email</label><br/>
-          <input class="form-input" type="email" placeholder="Email"><br/>
-          <input class="form-submit" type="submit">
-          <input class="form-reset" type="reset">
-        </form>
-      </div>
-      <div class="second-fifth-section">
-        <img src="images/gens-form.jpg" class="form-image">
-      </div>
+        <div class="first-fifth-section">
+            <form class="form" action="main.php" method="post">
+                <h1 class="form-title">Get in touch with me !</h1>
+                <label class="form-name" for="fullname">Your Name</label><br />
+                <input class="form-input" type="text" name="fullname" id="fullname" placeholder="Fullname"><br /><br />
+                <label class="form-email" for="email">Your Email</label><br />
+                <input class="form-input" type="email" name="email" id="email" placeholder="Email"><br />
+                <input class="form-submit" type="submit" name="submit">
+                <input class="form-reset" type="reset">
+            </form>
+        </div>
+        <div class="second-fifth-section">
+            <img src="images/gens-form.jpg" class="form-image">
+        </div>
     </div>
     <footer>
-      <p class="footer">Genesaret Johnes</p>
-      <p class="child-footer">see you next time.</p>
+        <p class="footer">Genesaret Johnes</p>
+        <p class="child-footer">see you next time.</p>
     </footer>
-    </body>
+</body>
+
 </html>
